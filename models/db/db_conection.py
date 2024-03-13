a6996960cfb985f14047d0445d9e90f5328838c4
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2024-03-13 17:20:37 +0000

    alteracoes projeto

--- models/db/db_conection.py
+++ models/db/db_conection.py
@@ -1,9 +1,22 @@
 from sqlmodel import SQLModel, create_engine
+from pathlib import os
+from dotenv import load_dotenv
 
+load_dotenv()
+  
 sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+url = os.getenv('SECRET_BD')
+sqlite_url = f"sqlite:///{url}"
 
 engine = create_engine(sqlite_url)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+"""sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)"""    
