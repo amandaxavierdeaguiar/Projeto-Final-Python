3767b135a4e8246be960055a112aa7ca0997545a
Fabio Rodrigues <fabio.rod@outlook.pt>
2024-03-18 19:58:44 +0000

    Fix stock data on CTkTable

--- views/StockView.py
+++ views/StockView.py
@@ -61,35 +61,15 @@ class StockView:
 
         # PRODUTOS(IMPORTAR DA DATABASE)
         table_data = cls.ctrl.get_all(cls.session)
-        result = [r[0] for r in table_data]
-
-        # table_data = [
-        #     ["Order ID", "Item Name", "Customer", "Address", "Status", "Quantity"],
-        #     ['3833', 'Smartphone', 'Alice', '123 Main St', 'Confirmed', '8'],
-        #     ['6432', 'Laptop', 'Bob', '456 Elm St', 'Packing', '5'],
-        #     ['2180', 'Tablet', 'Crystal', '789 Oak St', 'Delivered', '1'],
-        #     ['5438', 'Headphones', 'John', '101 Pine St', 'Confirmed', '9'],
-        #     ['9144', 'Camera', 'David', '202 Cedar St', 'Processing', '2'],
-        #     ['7689', 'Printer', 'Alice', '303 Maple St', 'Cancelled', '2'],
-        #     ['1323', 'Smartwatch', 'Crystal', '404 Birch St', 'Shipping', '6'],
-        #     ['7391', 'Keyboard', 'John', '505 Redwood St', 'Cancelled', '10'],
-        #     ['4915', 'Monitor', 'Alice', '606 Fir St', 'Shipping', '6'],
-        #     ['5548', 'External Hard Drive', 'David', '707 Oak St', 'Delivered', '10'],
-        #     ['5485', 'Table Lamp', 'Crystal', '808 Pine St', 'Confirmed', '4'],
-        #     ['7764', 'Desk Chair', 'Bob', '909 Cedar St', 'Processing', '9'],
-        #     ['8252', 'Coffee Maker', 'John', '1010 Elm St', 'Confirmed', '6'],
-        #     ['2377', 'Blender', 'David', '1111 Redwood St', 'Shipping', '2'],
-        #     ['5287', 'Toaster', 'Alice', '1212 Maple St', 'Processing', '1'],
-        #     ['7739', 'Microwave', 'Crystal', '1313 Cedar St', 'Confirmed', '8'],
-        #     ['3129', 'Refrigerator', 'John', '1414 Oak St', 'Processing', '5'],
-        #     ['4789', 'Vacuum Cleaner', 'Bob', '1515 Pine St', 'Cancelled', '10']
-        # ]
+        result = ["Bar Code", "Product Name", "Brand", "Category", "Price", "Quantity"]
+        table_data.insert(0, result)
 
         # Definições Tabela
         table_frame = CTkScrollableFrame(master=frame, fg_color="transparent")
         table_frame.pack(expand=True, fill="both", padx=27, pady=21)
-        table = CTkTable(master=table_frame, values=result, colors=["#E6E6E6", "#EEEEEE"],
+        table = CTkTable(master=table_frame, values=table_data, colors=["#E6E6E6", "#EEEEEE"],
                          header_color="#008DD2", hover_color="#B4B4B4")
         table.edit_row(0, text_color="#fff", hover_color="#2A8C55")
         table.pack(expand=True)
+
         return frame
