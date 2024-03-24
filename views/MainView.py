26390629341967448810413376d6a14af5a426b2
Fabio Rodrigues <fabio.rod@outlook.pt>
2024-03-24 20:33:04 +0000

    Fornecedores e Users View completa

--- views/MainView.py
+++ views/MainView.py
@@ -9,119 +9,126 @@ from ttkbootstrap.constants import *
 from models.UserAuthentication import UserAuthentication
 from views.StockView import StockView
 from views.SupplierView import SupplierView
+from views.UserView import UserView
 
 PATH = Path(__file__).parent / "assets"
 
 
 class MainView(ttk.Frame):
     root = None
-    error_txt: str = ""
+    user: UserAuthentication
     menu_frame: ttk.Frame
     main_frame: ttk.Frame
-    user: UserAuthentication
     stock: StockView
     supplier: SupplierView
+    users: UserView
     img_logo: ImageTk.PhotoImage
     button1: Button
     button2: Button
     button3: Button
     button4: Button
+    button5: Button
 
     def __init__(self, master, user_: UserAuthentication):
         super().__init__(master, padding=(10, 5))
         self.root = master
         self.user = user_
         self.frames_nav()
-        self.menu(user_)
-        self.stock = StockView(master, user_)
-        self.main_frame = self.stock.get_frame(user_)
-        self.pack(fill=BOTH, expand=YES)
+        self.menu()
+        # self.pack(fill=BOTH, expand=YES)
+        my_style = ttk.Style("cosmo")
+        my_style.configure(
+            "My.TButton", cursor="hand2", compound=tk.LEFT, font=("Verdana", 14)
+        )
 
-    @classmethod
-    def frames_nav(cls):
-        cls.menu_frame = ttk.Frame(cls.root, width=176, height=30, style=PRIMARY)
-        cls.menu_frame.pack(fill=BOTH, side="left", expand=False)
+    def frames_nav(self):
+        self.menu_frame = ttk.Frame(self.root, width=170, height=30, style=PRIMARY)
+        self.menu_frame.pack(fill=BOTH, side="left", expand=False)
+        self.stock = StockView(self.root, self.user)
+        self.main_frame = self.stock.get_frame(self.user)
+        self.main_frame.pack(fill=BOTH, side="left", expand=True)
 
     @classmethod
     def create_button(cls, frame_, img_, text_, command_, row_, disabled_) -> Button:
         img_path_temp = Image.open(PATH / img_)
         img_temp = ImageTk.PhotoImage(img_path_temp.resize((30, 35)))
         if disabled_:
-            btn_temp = Button(
+            btn_temp = ttk.Button(
                 master=frame_,
-                width=170,
-                height=75,
+                width=30,
+                # height=75,
                 cursor="hand2",
                 image=img_temp,
                 compound=tk.LEFT,
                 command=command_,
                 text=text_,
                 state="disabled",
-                font=("Verdana", 12),
+                # font=("Verdana", 12),
             )
             btn_temp.grid(column=0, row=row_)
             return btn_temp
         else:
-            btn_temp = Button(
+            btn_temp = ttk.Button(
                 master=frame_,
-                width=170,
-                height=75,
-                cursor="hand2",
+                width=30,
+                # height=75,
                 image=img_temp,
                 compound=tk.LEFT,
                 command=command_,
                 text=text_,
-                font=("Verdana", 12),
+                default="normal",
+                state="normal",
+                style="my.TButton",
             )
             btn_temp.grid(column=0, row=row_)
             return btn_temp
 
-    @classmethod
-    def menu(cls, user_):
+    def menu(self):
         img_logo = Image.open(PATH / "logo-stock-b.png")
-        cls.img_logo = ImageTk.PhotoImage(img_logo.resize((100, 100)))
-        cls.button1 = Button(
-            cls.menu_frame, width=172, height=230, image=cls.img_logo, text=""
+        self.img_logo = ImageTk.PhotoImage(img_logo.resize((100, 100)))
+        self.button1 = Button(
+            self.menu_frame,
+            width=172,
+            height=230,
+            image=self.img_logo,
+            text="",
         )
-        cls.button1.grid(column=0, row=0)
+        self.button1.grid(column=0, row=0)
 
-        cls.button2 = cls.create_button(
-            cls.menu_frame, "list.png", "Stock", cls.call_stock, 1, False
+        self.button2 = self.create_button(
+            self.menu_frame, "list.png", "Stock", self.call_stock, 1, False
         )
-        cls.button3 = cls.create_button(
-            cls.menu_frame, "supplier.png", "Fornecedor", cls.call_supplier, 2, False
+        self.button3 = self.create_button(
+            self.menu_frame, "supplier.png", "Fornecedor", self.call_supplier, 2, False
         )
 
-        if "Read" in user_.permissions["Users"]:
-            cls.button4 = cls.create_button(
-                cls.menu_frame, "user.png", "Users", cls.call_supplier, 3, False
+        if "Read" in self.user.permissions["User"]:
+            self.button4 = self.create_button(
+                self.menu_frame, "user.png", "Users", self.call_user, 3, False
             )
         else:
-            cls.button4 = cls.create_button(
-                cls.menu_frame, "user.png", "Users", cls.call_supplier, 3, True
+            self.button4 = self.create_button(
+                self.menu_frame, "user.png", "Users", self.call_user, 3, True
             )
 
-        cls.button5 = cls.create_button(
-            cls.menu_frame, "exit.png", "Logout", cls.call_exit, 5, False
+        self.button5 = self.create_button(
+            self.menu_frame, "exit.png", "Logout", self.call_logout, 5, False
         )
 
-    @classmethod
-    def logout(cls):
-        cls.menu_frame.forget()
-        # cls.login.window()
+    def call_logout(self):
+        self.destroy()
 
-    @classmethod
-    def call_stock(cls):
-        cls.menu_frame.forget()
-        cls.stock = StockView(cls.root, cls.user)
-        cls.menu_frame = cls.stock.get_frame()
+    def call_stock(self):
+        self.main_frame.destroy()
+        self.stock = StockView(self.root, self.user)
+        self.main_frame = self.stock.get_frame(self.user)
 
-    @classmethod
-    def call_supplier(cls):
-        cls.menu_frame.forget()
-        cls.supplier = SupplierView(cls.root, cls.user)
-        # cls.main_frame = cls.supplier.get_frame()
+    def call_supplier(self):
+        self.main_frame.destroy()
+        self.supplier = SupplierView(self.root, self.user)
+        self.main_frame = self.supplier.get_frame(self.user)
 
-    @classmethod
-    def call_exit(cls):
-        cls.menu_frame.destroy()
+    def call_user(self):
+        self.main_frame.destroy()
+        self.users = UserView(self.root, self.user)
+        self.main_frame = self.users.get_frame(self.user)
